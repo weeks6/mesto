ffaba9e1300e09cea70a31b82a0edecea7c5199e
weeks6 <bridcarp1@gmail.com>
2021-01-29 23:59:28 +0300

    загрузка карточек из скрипта

--- pages/index.js
+++ pages/index.js
@@ -44,3 +44,44 @@ const closeEditPopup = () => {
 editButton.addEventListener('click', openEditPopup)
 closeButton.addEventListener('click', closeEditPopup)
 editForm.addEventListener('submit', submitEditForm)
+
+
+// card rendering
+const initialCards = [
+    {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+  ]; 
+
+const cardList = document.querySelector('.elements')
+const cardTemplate = document.querySelector('#card-template').content
+
+for (let cardContent of initialCards) {
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true)
+
+    cardElement.querySelector('.card__image').src = cardContent.link
+    cardElement.querySelector('.card__title').textContent = cardContent.name
+
+    cardList.append(cardElement)
+}
